b22341a2ea9a7e99f39ed9e60a603569fbb1094d
Jovells <jovells.appiah@gmail.com>
2024-08-30 11:36:46 +0000

    fixed graph url

--- packages/nextjs/app/constants.ts
+++ packages/nextjs/app/constants.ts
@@ -1,1 +1,1 @@
-export const THE_GRAPH_URL = "https://api.studio.thegraph.com/query/74011/duniverse/latest";
+export const THE_GRAPH_URL = "https://api.studio.thegraph.com/query/74011/duniverse/version/latest";
