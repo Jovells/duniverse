b8d14ecdcf6d34cd7b8bf79cea1df462ac9c56a4
Jovells <jovells.appiah@gmail.com>
2024-08-30 14:23:04 +0000

    configured chains

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,5 +1,6 @@
 import * as chains from "viem/chains";
 
+
 export type ScaffoldConfig = {
   targetNetworks: readonly chains.Chain[];
   pollingInterval: number;
@@ -10,7 +11,8 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  targetNetworks: [chains.sepolia, chains.zkSyncSepoliaTestnet, chains.liskSepolia],
+  // targetNetworks: [chains.sepolia, chains.zkSyncSepoliaTestnet, chains.liskSepolia, chains.hardhat],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
